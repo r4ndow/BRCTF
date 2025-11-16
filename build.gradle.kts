f472828ada78d35dc52242dfc1a872c16c7639e2
nomnuggetnom <nomnuggetnom@gmail.com>
2025-11-15 18:26:05 -0700

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("com.github.zafarkhaja:java-semver:0.9.0")
 }
 
-val pluginVersion = "3.4.0"
+val pluginVersion = "3.4.1"
 
 group = "com.mcctf"
 version = pluginVersion
