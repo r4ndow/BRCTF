c5dd11c6f0288e8725e5cd471d6bfafd47e9f5aa
nomnuggetnom <nomnuggetnom@gmail.com>
2025-11-23 12:40:49 -0700

    Allow switching other players

--- src/main/java/com/mcpvp/battle/command/SwitchCommand.java
+++ src/main/java/com/mcpvp/battle/command/SwitchCommand.java
@@ -3,8 +3,11 @@ package com.mcpvp.battle.command;
 import com.mcpvp.battle.Battle;
 import com.mcpvp.battle.team.BattleTeam;
 import com.mcpvp.battle.team.BattleTeamManager;
+import com.mcpvp.common.chat.C;
 import com.mcpvp.common.command.EasyCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -19,10 +22,28 @@ public class SwitchCommand extends EasyCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, String label, List<String> args) {
+        Player target = this.asPlayer(sender);
+
+        // Allow switching other players
+        if (!args.isEmpty()) {
+            if (!sender.hasPermission("mcctf.switch.other")) {
+                return false;
+            }
+
+            Player player = Bukkit.getPlayer(args.get(0));
+            if (player == null || !this.battle.getGame().isParticipant(player)) {
+                sender.sendMessage(C.cmdFail() + "This player is offline or not participating.");
+                return false;
+            }
+
+            target = player;
+        }
+
+        // The message is sent by the PlayerJoinTeamEvent via BattleGameListener
         BattleTeamManager teamManager = this.battle.getGame().getTeamManager();
-        BattleTeam current = teamManager.getTeam(this.asPlayer(sender));
+        BattleTeam current = teamManager.getTeam(target);
         BattleTeam next = teamManager.getNext(current);
-        teamManager.setTeam(this.asPlayer(sender), next);
+        teamManager.setTeam(target, next);
         return true;
     }
 
