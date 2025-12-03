269ff113c7967916d99d6d6e6c9c323ec86a1609
nomnuggetnom <nomnuggetnom@gmail.com>
2025-12-02 18:43:48 -0700

    Remember last compass direction

--- src/main/java/com/mcpvp/battle/kit/item/FlagCompassItem.java
+++ src/main/java/com/mcpvp/battle/kit/item/FlagCompassItem.java
@@ -10,39 +10,58 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FlagCompassItem extends KitItem {
+
+    private static final Map<Player, BattleTeam> TARGETS = new HashMap<>();
 
     private final BattleGame game;
     private final Player player;
-    private BattleTeam target;
 
     public FlagCompassItem(Kit kit, BattleGame game) {
         super(kit, ItemBuilder.of(Material.COMPASS).name("Pointing to ??? flag").build());
         this.game = game;
         this.player = kit.getPlayer();
-        this.target = game.getTeamManager().getTeam(kit.getPlayer());
         this.onInteract(ev -> this.toggle());
-        this.toggle();
+
+        if (this.getTarget() == null) {
+            this.setTarget(this.game.getTeamManager().getTeam(this.player));
+        }
+
+        this.rename();
     }
 
     public void toggle() {
-        if (this.target != null) {
-            this.target = this.game.getTeamManager().getNext(this.target);
-            if (this.target == this.game.getTeamManager().getTeam(this.player)) {
-                this.modify(ib -> ib.name("Pointing to " + this.target.getColoredName() + " flag").dummyEnchant());
-            } else {
-                this.modify(ib -> ib.name("Pointing to " + this.target.getColoredName() + " flag").removeDummyEnchant());
-            }
-
-            this.update(this.kit.getPlayer().getInventory());
+        if (this.getTarget() != null) {
+            this.setTarget(this.game.getTeamManager().getNext(this.getTarget()));
+            this.rename();
         }
+    }
+
+    private void rename() {
+        if (this.getTarget() == this.game.getTeamManager().getTeam(this.player)) {
+            this.modify(ib -> ib.name("Pointing to " + this.getTarget().getColoredName() + " flag").dummyEnchant());
+        } else {
+            this.modify(ib -> ib.name("Pointing to " + this.getTarget().getColoredName() + " flag").removeDummyEnchant());
+        }
+        this.update(this.kit.getPlayer().getInventory());
     }
 
     @EventHandler
     public void onTick(TickEvent event) {
-        if (this.target != null && this.kit.getPlayer().getCompassTarget() != this.target.getFlag().getLocation()) {
-            this.kit.getPlayer().setCompassTarget(this.target.getFlag().getLocation());
+        if (this.getTarget() != null && this.kit.getPlayer().getCompassTarget() != this.getTarget().getFlag().getLocation()) {
+            this.kit.getPlayer().setCompassTarget(this.getTarget().getFlag().getLocation());
         }
+    }
+
+    private BattleTeam getTarget() {
+        return TARGETS.get(this.player);
+    }
+
+    private void setTarget(BattleTeam target) {
+        TARGETS.put(this.player, target);
     }
 
 }
