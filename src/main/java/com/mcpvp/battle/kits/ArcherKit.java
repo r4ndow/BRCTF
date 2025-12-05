168f9227e62dc91a7bf072496ecadb90e7cd0833
r4ndow <r4ndow01@gmail.com>
2025-12-05 15:14:34 -0300

    Archer balancing: 30-block insta-kill only against archers, soldiers or enemies close to the flag

--- src/main/java/com/mcpvp/battle/kits/ArcherKit.java
+++ src/main/java/com/mcpvp/battle/kits/ArcherKit.java
@@ -6,6 +6,7 @@ import com.mcpvp.common.InteractiveProjectile;
 import com.mcpvp.common.chat.C;
 import com.mcpvp.common.item.ItemBuilder;
 import com.mcpvp.common.kit.KitItem;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.*;
@@ -49,7 +50,7 @@ public class ArcherKit extends BattleKit {
             .add(ItemBuilder.of(Material.STONE_SWORD)
                 .name("Archer Sword")
                 .unbreakable())
-            .addFood(2)
+            .addFood(4)
             .add(ItemBuilder.of(Material.BOW)
                 .name("Archer Bow")
                 .enchant(Enchantment.ARROW_KNOCKBACK, 1)
@@ -100,18 +101,23 @@ public class ArcherKit extends BattleKit {
             return;
         }
 
-        if (!(event.getDamager() instanceof Arrow arrow)) {
+        if (!(event.getDamager() instanceof Arrow)) {
             return;
         }
 
-        if (!(arrow.getShooter() instanceof Player shooter)) {
+        Arrow arrow = (Arrow) event.getDamager();
+
+        if (!(arrow.getShooter() instanceof Player)) {
             return;
         }
 
-        if (!(event.getEntity() instanceof LivingEntity hit)) {
+        Player shooter = (Player) arrow.getShooter();
+
+        if (!(event.getEntity() instanceof LivingEntity)) {
             return;
         }
 
+        LivingEntity hit = (LivingEntity) event.getEntity();
         int distance = (int) hit.getLocation().distance(shooter.getLocation());
 
         if (hit instanceof ArmorStand) {
@@ -122,10 +128,40 @@ public class ArcherKit extends BattleKit {
             return;
         }
 
+        if (hit instanceof Player) {
+            Player hitPlayer = (Player) hit;
+
+            if (!this.isEnemy(hitPlayer)) {
+                return;
+            }
+
+            boolean instaKill = false;
+
+            BattleKit targetKit = this.getBattle().getKitManager().get(hitPlayer);
+            if (targetKit instanceof ArcherKit || targetKit instanceof SoldierKit) {
+                instaKill = true;
+            }
+
+            if (!instaKill && this.getTeam() != null && this.getTeam().getFlag() != null) {
+                Location flagLocation = this.getTeam().getFlag().getLocation();
+                if (flagLocation != null && hitPlayer.getLocation().distance(flagLocation) <= 10) {
+                    instaKill = true;
+                }
+            }
+
+            if (!instaKill) {
+                return;
+            }
+        }
+
         event.setDamage(1000);
 
-        hit.sendMessage(C.info(C.GOLD) + "You were sniped by " + C.highlight(shooter.getName()) + " from " + distance + " blocks!");
-        shooter.sendMessage(C.info(C.GOLD) + "You sniped " + C.highlight(hit.getName()) + " from " + distance + " blocks!");
+        if (hit instanceof Player) {
+            Player hitPlayer = (Player) hit;
+            hitPlayer.sendMessage(C.info(C.GOLD) + "You were sniped by " + C.hl(shooter.getName()) + " from " + distance + " blocks!");
+            shooter.sendMessage(C.info(C.GOLD) + "You sniped " + C.hl(hitPlayer.getName()) + " from " + distance + " blocks!");
+        }
     }
+
 
 }
